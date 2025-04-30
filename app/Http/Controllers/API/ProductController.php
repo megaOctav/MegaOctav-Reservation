dc7d80940cb178214163c4d573d0cf463ce2c387
megaOctav <imeg290@gmail.com>
2025-04-30 10:37:39 +0700

    update product

--- app/Http/Controllers/API/ProductController.php
+++ app/Http/Controllers/API/ProductController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class CategoryController extends Controller
+class ProductController extends Controller
 {
     /**
      * @OA\Get(
