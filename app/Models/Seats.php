ee7aa49adf196100aeccc86e84f5e531adb3925a
TiaraNafisaa <urfvgirliex@gmail.com>
2025-05-23 07:49:09 +0700

    Membuat tabel Seats

---
+++ app/Models/Seats.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Seat extends Model
+{
+    protected $primaryKey = 'id_seats';
+
+    protected $fillable = [
+        'schedule_id',
+        'number',
+        'status_seats'
+    ];
+
+    public function schedule()
+    {
+        return $this->belongsTo(Schedule::class, 'schedule_id', 'schedule_id');
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -11,6 +11,7 @@ use App\Http\Controllers\API\ScheduleController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\UserController;
+use App\Http\Controllers\API\SeatsController;
 
 
 Route::get('/user', function (Request $request) {
@@ -33,6 +34,15 @@ Route::prefix('schedules')->group(function () {
     Route::post('/', [ScheduleController::class, 'store']);        // POST tambah schedule
     Route::put('/{id}', [ScheduleController::class, 'update']);    // PUT update schedule
     Route::delete('/{id}', [ScheduleController::class, 'destroy']); // DELETE schedule
+});
+
+//Seats
+Route::prefix('seats')->group(function () {
+Route::get('/seats', [SeatsController::class, 'index']);
+Route::post('/seats', [SeatsController::class, 'store']);
+Route::get('/seats/{id}', [SeatsController::class, 'show']);
+Route::put('/seats/{id}', [SeatsController::class, 'update']);
+Route::delete('/seats/{id}', [SeatsController::class, 'destroy']);
 });
 
 
